a5734b9f1a67db054f44c3c521b5d548a21c1c78
comraq <yinadam@hotmail.com>
2015-12-07 22:09:01 -0800

    more messing around with query

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,17 @@
 $(document).ready(function() {
-  $('#title').mouseenter(function() {
-    $('#title').fadeTo('slow', 1);
+  $("#title").mouseenter(function() {
+    $("#title").fadeTo('slow', 1);
   });
 
-  $('#title').mouseleave(function() {
-    $('#title').fadeTo('slow', 0.3);
-  });  
+  $("#title").mouseleave(function() {
+    $("#title").fadeTo('slow', 0.3);
+  }); 
+
+  $("button").click(function() {
+    $(".wip-description").hide();
+  }); 
+
+  $(".wip").mouseleave(function() {
+    $(".wip-description").show();
+  });
 });
