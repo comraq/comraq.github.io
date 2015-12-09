da39245c6dd071d0783511b7a816bdf342304ff2
comraq <yinadam@hotmail.com>
2015-12-08 21:11:00 -0800

    using js to practice some algorithm questions

--- js/custom.js
+++ js/custom.js
@@ -8,10 +8,56 @@ $(document).ready(function() {
   }); 
 
   $("button").click(function() {
-    $(".wip-description").hide();
+    $(".wip-description").show();
   }); 
 
   $(".wip").mouseleave(function() {
-    $(".wip-description").show();
+    $(".wip-description").hide();
   });
+
+  $("#algorithm").click(function() {
+    stockProfit();
+  });  
 });
+
+function stockProfit() {
+  var array = [2, 1, -4, -7, 9, 3, 10, -7, -3, 4, 7, 2, 5, -1, -3, 8]; 
+  var arrayDiff = [0];
+  for (var i = 1; i < array.length; ++i) {
+    arrayDiff.push(array[i] - array[i-1]);
+  }  
+  confirm("Original array: [" + array + "]\n" +
+           subArrayMaxSum(arrayDiff));
+};
+
+function subArrayMaxSum(array) {
+  var max = 0;
+  var sum = 0;
+  var start = 0;
+  var tempStart = start;
+  var end = start;
+
+  for (var i = 0; i < array.length; ++i) {
+    if (array[i] < 0) {
+      if (sum > max) {
+        max = sum;
+        end = i - 1;
+        start = tempStart;
+      }
+    }
+    sum += array[i];
+    if (max + sum < 0) {
+      sum = 0;
+      tempStart = i + 1;
+    }
+  };
+
+  if (sum > max) {
+    max = sum;
+    end = i - 1;
+  }
+
+  return("Diff array: [" + array + "]\n\n" +
+         "SubArrayMaxSum: " + max + "\n" + 
+         "StartIndex: " + start + " EndIndex: " + end);
+};
